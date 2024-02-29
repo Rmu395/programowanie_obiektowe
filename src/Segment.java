cbb18142edab74ffec75bbf716663ddd9022d6dc
Rmu395 <kgdula7@gmail.com>
2024-02-29 15:34:58 +0100

    Zadanie 2

---
+++ src/Segment.java
@@ -0,0 +1,21 @@
+public class Segment {
+    private Point p1, p2;
+
+    public Point getP1() {
+        return p1;
+    }
+
+    public Point getP2() {
+        return p2;
+    }
+
+    public Segment(Point p1, Point p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    public double getDistance() {
+        return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+        //Math.hypot(p1.x - p2.x, p1.y - p2.y);
+    }
+}
