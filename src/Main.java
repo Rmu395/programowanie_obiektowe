dedd29658b0917dfeae17593e5a40adb9412302e
Rmu395 <kgdula7@gmail.com>
2024-02-29 23:00:12 +0100

    Test commit

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,12 @@ public class Main {
     public static void main(String[] args) {
         Point p1 = new Point(10, 10);
         Point p2 = new Point(100, 100);
+
         Segment segment = new Segment(p1, p2);
+
         System.out.println(segment.toSvg());
 
+        // Tablica
         Point[] points = new Point[10];
     }
 }
