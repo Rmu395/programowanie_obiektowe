9fe1429791ca228c4d557c7be97a4fb5b04cd8a9
Rmu395 <kgdula7@gmail.com>
2024-03-07 14:12:19 +0100

    Test commit

--- src/Polygon.java
+++ src/Polygon.java
@@ -7,7 +7,16 @@ public class Polygon {
         this.points = points;
     }
 
+    public Polygon(Polygon copySrc) {
+        this.points = copySrc.points;
+    }
+
+
     public String toSvg() {
-        return String.format(Locale.ENGLISH, "<polygon points=\"%f,%f %f,%f %f,%f %f,%f\" style=\"stroke-width:5\" />", points[0].x, points[0].y, points[1].x, points[1].y, points[2].x, points[2].y, points[3].x, points[3].y);
+        String polygonSvg = "<polygon points=\"";
+        for(Point point: points)
+            polygonSvg += point.x + "," + point.y + " ";
+        polygonSvg += "\" />";
+        return polygonSvg;
     }
 }
