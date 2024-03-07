d046d8c984f55687a93bd0645f4486a681ca76b3
Rmu395 <kgdula7@gmail.com>
2024-03-07 15:21:41 +0100

    Nadrabianie (żeby nie być w tyle na zajęciach)

--- src/Polygon.java
+++ src/Polygon.java
@@ -1,22 +1,27 @@
 import java.util.Locale;
 
 public class Polygon {
-    private Point[] points;
+    private Point[] arr;
 
-    public Polygon(Point[] points) {
-        this.points = points;
+    public Polygon(int count) {
+        arr = new Point[count];
     }
 
-    public Polygon(Polygon copySrc) {
-        this.points = copySrc.points;
+    public void setPoint(int index, Point point) {
+        arr[index] = point;
     }
 
+    public void setPoints(Point[] points){
+        arr = points;
+    }
 
     public String toSvg() {
-        String polygonSvg = "<polygon points=\"";
-        for(Point point: points)
-            polygonSvg += point.x + "," + point.y + " ";
-        polygonSvg += "\" />";
-        return polygonSvg;
+        String pointsString = "";
+        for(Point point : arr)
+            pointsString += point.x + "," + point.y + " ";
+
+        return String.format(Locale.ENGLISH,"<polygon points=\"%s\" />", pointsString);
     }
+
+
 }
